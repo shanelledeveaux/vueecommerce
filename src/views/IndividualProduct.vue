122a9efb0d18f03d879317f77293e68c3f326226
shanellewebster <webstershanelle@gmail.com>
2019-02-26 20:41:23 -0600

    routes

---
+++ src/views/IndividualProduct.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="shop">
+    <h1>This is the individual product</h1>
+  </div>
+</template>
---
+++ src/views/Shop.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="shop">
+    <h1>This is the shopping page</h1>
+  </div>
+</template>
