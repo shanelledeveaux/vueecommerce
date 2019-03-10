5c669344f08749ca7df3982bcc2f0ebdc9294668
shanellewebster <webstershanelle@gmail.com>
2019-03-10 11:43:20 -0500

    basic card and grid

--- src/components/smallProductCard.vue
+++ src/components/smallProductCard.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class=".product-card">
-    <h1>Small Product Card</h1>
+  <div class="product-card">
+      <img
+          width="200px"
+          v-bind:src="product.imageurl"
+        >
+    <h1>{{product.name}}</h1>
+    <div>{{product.price}}</div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["product"]
+};
+</script>
+
+<style scoped></style>
