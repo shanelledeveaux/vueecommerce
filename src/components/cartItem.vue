28fe8c706583a9a6f3bc5d52b3153f686a80cb4f
shanellewebster <webstershanelle@gmail.com>
2019-04-24 20:41:27 -0500

    some cart styling

--- src/components/cartItem.vue
+++ src/components/cartItem.vue
@@ -1,9 +1,15 @@
 <template>
 
-  <div>
+  <div class="cart-item">
+      <img
+          width="200px"
+          v-bind:src="item.imageurl"
+        >
     <h1>{{item.name}}</h1>
     <div>{{item.price}}</div>
     <div>{{item.quantity}}</div>
+    <button>edit</button>
+    <button>trash</button>
   </div>
 
 </template>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is a cart page</h1>
+    <div class="cart-item-container">
     <cartItem
       v-for="item in cart"
       v-bind:item="item"
       v-bind:key="item.cart_item_id"
     >
     </cartItem>
+    </div>
+    <div>
+      <orderSummary/>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import cartItem from "../components/cartItem";
+import orderSummary from "../components/orderSummary";
 export default {
   components: {
-    cartItem
+    cartItem,
+    orderSummary
   },
 
   data: () => ({}),
