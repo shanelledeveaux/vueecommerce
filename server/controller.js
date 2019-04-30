33012de488b91cbfc53e7139d9e69a6c3290153f
shanellewebster <webstershanelle@gmail.com>
2019-04-29 21:25:37 -0500

    need to chage db structure to support integer

--- server/controller.js
+++ server/controller.js
@@ -10,9 +10,24 @@ const getInventory = (req, res) => {
 const getCart = (req, res) => {
   const dbInstance = req.app.get("db");
 
+  let subtotal = 0;
+  const cart = [];
+
   dbInstance
     .get_cart()
-    .then(response => res.status(200).send(response))
+    .then(
+      response =>
+        Object.keys(response).forEach(key => {
+          subtotal = subtotal + parseInt(response[key].price);
+          console.log(response[key].price);
+          cart.push(response[key]);
+        }),
+      (cart.subtotal = subtotal),
+      (cart.tax = subtotal * 0.0825),
+      (cart.orderTotal = subtotal * 1.0825),
+      console.log(cart)
+    )
+    .then(cart => console.log(cart))
     .catch(console.log);
 };
 
