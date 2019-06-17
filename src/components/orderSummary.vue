6653843b9fd25ce36d35096dda13d7abf946615a
shanellewebster <webstershanelle@gmail.com>
2019-06-16 21:31:07 -0500

    added payment details section

--- src/components/orderSummary.vue
+++ src/components/orderSummary.vue
@@ -1,28 +1,60 @@
 <template>
   <div class="cart-summary">
+    <div>
     <h1>
         Order Summary
     </h1>
-    <div>
-      <div class="flexbox">
-        <h4>Subtotal</h4>
+    <div class="cart-totals-wrapper">
+      <div class="cart-total-line-item">
+        <label>Subtotal</label>
         <span>{{subtotal}}</span>
       </div>
-      <div class="flexbox">
-        <h4>Estimated Shipping</h4>
+      <div class="cart-total-line-item">
+        <label>Estimated Shipping</label>
         <span>Free</span>
       </div>
-      <div class="flexbox">
-        <h4>Tax</h4>
+      <div class="cart-total-line-item">
+        <label>Tax</label>
         <span>{{tax}}</span>
       </div>
-      <div class="flexbox">
-        <h2>Total</h2> 
+      <div class="cart-total-line-item">
+        <label>Total</label> 
         <span>{{orderTotal}}</span>
       </div>
     </div>
-    <button class="button">Checkout</button>
+    </div>
+    <!-- START PAYMENT DETAILS SECTION -->
+    <div>
+      <h1>Payment Details</h1>
+      <div>
+        <div>
+          <label>
+            Card Type:
+          </label>
+          <input>
+        </div>
+        <div>
+          <label>Card Number:</label>
+          <input>
+        </div>
+        <div>
+          <label>
+            Card Holder Name:
+          </label>
+          <input>
+        </div>
+        <div>
+          <label>ExpirationDate:</label>
+          <input>
+          <label>CVV:</label>
+          <input>
+        </div>
+      </div>
+      <button>Place Order</button>
+    </div>
   </div>
+
+
 </template>
 
 <script>
